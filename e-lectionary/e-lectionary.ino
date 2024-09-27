e88c219ffcf39a4e59e8f7c51797870457b85589
reZach <reZach@users.noreply.github.com>
2024-09-27 00:03:54 -0500

    optimizing a bit

--- e-lectionary/e-lectionary.ino
+++ e-lectionary/e-lectionary.ino
@@ -2,7 +2,7 @@
 // busy = 25 , rst = 26 , dc = 27 , cs = 15 , clk = 13 , din = 14 , VCC = 3V3, GND
 
 /* ----- RTC Pins ----- */
-//SCL = 22 , SDA = 21 , VCC = VIN, GND = GN
+// SCL = 22 , SDA = 21 , VCC = VIN, GND = GN
 
 #include <Wire.h>
 #include "RTClib.h"
@@ -13,63 +13,63 @@
 #include "data.h"
 
 /* ----- Constants ----- */
-const int MAX_TEXT_LENGTH = 20;
-const int TEXT_X_POS_DEFAULT = 110;
-const int TEXT_X_POS_ADJUSTED = 75;
-const int TEXT_X_POS_MIN = 50;
 const int EPD_WIDTH = EPD_4IN2_WIDTH;
 const int EPD_HEIGHT = EPD_4IN2_HEIGHT;
 
 /* ----- Variables ----- */
-String formatted_date;  //to store date in "December 5th, 2024" format (to show on display)
-String current_date;    //to store date in "2024-01-01" format (to compare with data.h)
-String row1 = "";       //showing rows on display
+String formatted_date; // to store date in "December 5th, 2024" format (to show on display)
+String current_date;   // to store date in "2024-01-01" format (to compare with data.h)
+String row1 = "";      // showing rows on display
 String row2 = "";
 String row3 = "";
 
-bool dateFound = false;  //to check if date is found in data.h
-int prevDay = 0;         //checking day condition
-int x = TEXT_X_POS_DEFAULT;
+bool dateFound = false; // to check if date is found in data.h
+int prevDay = 0;        // checking day condition
 
-String monthNames[] = { "January", "February", "March", "April", "May", "June",
-                        "July", "August", "September", "October", "November", "December" };
+String monthNames[] =
+    {"January", "February", "March", "April", "May", "June",
+     "July", "August", "September", "October", "November", "December"};
 
-String dayNames[] = {
-  "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th",
-  "11th", "12th", "13th", "14th", "15th", "16th", "17th", "18th", "19th", "20th",
-  "21st", "22nd", "23rd", "24th", "25th", "26th", "27th", "28th", "29th", "30th",
-  "31st"
-};
+String dayNames[] =
+    {"1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th",
+     "11th", "12th", "13th", "14th", "15th", "16th", "17th", "18th", "19th", "20th",
+     "21st", "22nd", "23rd", "24th", "25th", "26th", "27th", "28th", "29th", "30th",
+     "31st"};
 
 /* -- Create a new image cache -- */
-UBYTE* BlackImage;
+UBYTE *BlackImage;
 
 /* ----- init RTC ----- */
 RTC_DS3231 rtc;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Serial.println("setup()");
 
-  if (!rtc.begin()) {
+  if (!rtc.begin())
+  {
     Serial.println("Couldn't find RTC");
-    while (1) delay(10);
+    while (1)
+      delay(10);
   }
-  
-  // rtc.adjust(DateTime(2070, 12, 16, 3, 0, 0));
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));    
 
-  DEV_Module_Init();  
-  EPD_4IN2_V2_Init(); //EPD_4IN2_Init_Fast();  
-  EPD_4IN2_V2_Clear(); //EPD_4IN2_Clear();  
-  DEV_Delay_ms(500);  
+  // rtc.adjust(DateTime(2070, 12, 16, 3, 0, 0));
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
+  DEV_Module_Init();
+  EPD_4IN2_V2_Init();  // EPD_4IN2_Init_Fast();
+  EPD_4IN2_V2_Clear(); // EPD_4IN2_Clear();
+  DEV_Delay_ms(500);
 }
 
-void loop() {
-  DateTime time = rtc.now();  
+void loop()
+{
+  DateTime time = rtc.now();
 
   /* -- check if day changes -- */
-  if (prevDay != time.day()) {
+  if (prevDay != time.day())
+  {
     prevDay = time.day();
     dateFound = false;
 
@@ -85,15 +85,21 @@ void loop() {
     char format_2[50];
     String cur_day;
     String cur_mon;
-    if (time.day() < 10) {
+    if (time.day() < 10)
+    {
       cur_day = "0" + String(time.day());
-    } else {
+    }
+    else
+    {
       cur_day = String(time.day());
     }
 
-    if (time.month() < 10) {
+    if (time.month() < 10)
+    {
       cur_mon = "0" + String(time.month());
-    } else {
+    }
+    else
+    {
       cur_mon = String(time.month());
     }
 
@@ -104,10 +110,11 @@ void loop() {
     processYearData(data, sizeof(data) / sizeof(data[0]), current_date);
     /* ------------------------------------------------- */
 
-    if (!dateFound) {
-      row1 = "Ez 24:15-23";
-      row2 = "";
-      row3 = "Mt 19:16-22";
+    if (!dateFound)
+    {
+      row1 = "Not found";
+      row2 = "Not found";
+      row3 = "Not found";
     }
 
     displayDate();
@@ -115,12 +122,13 @@ void loop() {
 }
 
 /* -- Function to display Formatted date and rows on Display -- */
-void displayDate() {
-  Serial.print("displayDate");
+void displayDate()
+{
   // Allocate memory for image buffer
   UWORD imageSize = ((EPD_WIDTH % 8 == 0) ? (EPD_WIDTH / 8) : (EPD_WIDTH / 8 + 1)) * EPD_HEIGHT;
-  BlackImage = (UBYTE*)malloc(imageSize);
-  if (BlackImage == NULL) {
+  BlackImage = (UBYTE *)malloc(imageSize);
+  if (BlackImage == NULL)
+  {
     Serial.println("Failed to allocate memory for BlackImage");
     return;
   }
@@ -128,54 +136,81 @@ void displayDate() {
   Paint_NewImage(BlackImage, EPD_WIDTH, EPD_HEIGHT, 0, WHITE);
   Paint_SelectImage(BlackImage);
   Paint_Clear(WHITE);
-  Paint_DrawString_EN(55, 25, formatted_date.c_str(), &Font24, WHITE, BLACK);
-  adjustTxt(row1.c_str());
-  Paint_DrawString_EN(x, 105, row1.c_str(), &Font20, WHITE, BLACK);  
-  adjustTxt(row2.c_str());  
-  Paint_DrawString_EN(x, 145, row2.c_str(), &Font20, WHITE, BLACK);  
-  adjustTxt(row3.c_str());  
-  Paint_DrawString_EN(x, 185, row3.c_str(), &Font20, WHITE, BLACK);  
-  EPD_4IN2_V2_Display(BlackImage);  
-  free(BlackImage);  
+
+  int xStartDate = getXForCenteredText(formatted_date.c_str(), EPD_WIDTH, 17);
+
+  // If any of the readings are > 28 characters, we need to shrink the
+  // font so the reading all stays on one line
+  int versePx = 14;
+  if (strlen(row1.c_str()) > 28 ||
+      strlen(row2.c_str()) > 28 ||
+      strlen(row3.c_str()) > 28)
+  {
+    versePx = 11;
+  }
+
+  int xStartRow1 = getXForCenteredText(row1.c_str(), EPD_WIDTH, versePx);
+  int xStartRow2 = getXForCenteredText(row2.c_str(), EPD_WIDTH, versePx);
+  int xStartRow3 = getXForCenteredText(row3.c_str(), EPD_WIDTH, versePx);
+
+  Paint_DrawString_EN(xStartDate, 25, formatted_date.c_str(), &Font24, WHITE, BLACK);
+
+  if (versePx == 14)
+  {
+    Paint_DrawString_EN(xStartRow1, 105, row1.c_str(), &Font20, WHITE, BLACK);
+    Paint_DrawString_EN(xStartRow2, 135, row2.c_str(), &Font20, WHITE, BLACK);
+    Paint_DrawString_EN(xStartRow3, 165, row3.c_str(), &Font20, WHITE, BLACK);
+  }
+  else if (versePx == 11)
+  {
+    Paint_DrawString_EN(xStartRow1, 105, row1.c_str(), &Font16, WHITE, BLACK);
+    Paint_DrawString_EN(xStartRow2, 135, row2.c_str(), &Font16, WHITE, BLACK);
+    Paint_DrawString_EN(xStartRow3, 165, row3.c_str(), &Font16, WHITE, BLACK);
+  }
+
+  EPD_4IN2_V2_Display(BlackImage);
+  free(BlackImage);
   BlackImage = NULL;
 }
 
-/* -- check text alignments -- */
-void adjustTxt(const char* cmd) {
-  int len = strlen(cmd);
-  if (len >= MAX_TEXT_LENGTH) {
-    x = TEXT_X_POS_MIN;
-  } else if (len > 15) {
-    x = TEXT_X_POS_ADJUSTED;
-  } else {
-    x = TEXT_X_POS_DEFAULT;
-  }
+// Get the x-value that the given string should render at if desiring centered-text.
+int getXForCenteredText(const char *s, int screenWidth, int fontSizeInPX)
+{
+  return (screenWidth - (fontSizeInPX * strlen(s))) / 2;
 }
 
 /* -- if current date is found in data.h extract its data in rows -- */
-String extractField(const String& line, int index) {
+String extractField(const String &line, int index)
+{
   int startIndex = 0;
   int endIndex = line.indexOf('&');
 
-  for (int i = 0; i < index; i++) {
-    if (endIndex == -1) return "";
+  for (int i = 0; i < index; i++)
+  {
+    if (endIndex == -1)
+      return "";
     startIndex = endIndex + 1;
     endIndex = line.indexOf('&', startIndex);
   }
 
-  if (endIndex == -1) endIndex = line.length();
+  if (endIndex == -1)
+    endIndex = line.length();
+
   return line.substring(startIndex, endIndex);
 }
 
 /* -- check if current date exists in data.h file -- */
-void processYearData(const char* data[], int dataSize, String date) {
-  for (int i = 0; i < dataSize; i++) {
+void processYearData(const char *data[], int dataSize, String date)
+{
+  for (int i = 0; i < dataSize; i++)
+  {
     String line(data[i]);
     Serial.println(line);
 
     String idx = extractField(line, 0);
 
-    if (idx == date) {
+    if (idx == date)
+    {
       Serial.print("Extracted date: ");
       Serial.println(idx);
       row1 = extractField(line, 1);
@@ -190,9 +225,12 @@ void processYearData(const char* data[], int dataSize, String date) {
       Serial.print("Row3: ");
       Serial.println(row3);
 
-      if (row1 == "-") row1 = "";
-      if (row2 == "-") row2 = "";
-      if (row3 == "-") row3 = "";
+      if (row1 == "-")
+        row1 = "";
+      if (row2 == "-")
+        row2 = "";
+      if (row3 == "-")
+        row3 = "";
       break;
     }
   }
